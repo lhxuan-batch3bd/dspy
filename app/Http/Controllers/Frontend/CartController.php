167cc2eba64cca3228fa3479a77470a2b3122b48
= Lương Bảo Tín <= tinpro67@gmail.com>
2020-05-01 14:48:29 +0700

    phpmailer

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -100,7 +100,7 @@ class CartController extends Controller
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
             $mail->Username   = 'luongbaotin2020@gmail.com';                     // SMTP username
-            $mail->Password   = 'Tinpro123';                               // SMTP password
+            $mail->Password   = 'Tinyeuthao123';                               // SMTP password
             $mail->SMTPSecure =  'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
@@ -115,7 +115,7 @@ class CartController extends Controller
             $mail->Body    = $body;
             $mail->AltBody = '';
             $mail->send();
-            echo 'Message has been sent';
+            echo 'Đã gửi đơn xác nhận';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
